bbf07328e5879b1d8fc6c9adc0fccfcc9d3d7281
om raghuwanshi <omrag103@gmail.com>
2024-10-08 12:29:45 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,11 +2,11 @@ def decode_message( s: str, p: str) -> bool:
 
 # write your code here
    a, b = len(s), len(p)
-   dp = [[False] * (b + 1) for _ in range(m + 1)]
+   dp = [[False] * (b + 1) for _ in range(a + 1)]
 
    dp[0][0] = True
     
-   for j in range(1, n + 1):
+   for j in range(1, b + 1):
         if p[j - 1] == '*':
             dp[0][j] = dp[0][j - 1]
 
