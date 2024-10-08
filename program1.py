0b40cf511d22ae3b3753c173232632c0b2a1cff4
om raghuwanshi <omrag103@gmail.com>
2024-10-08 12:37:57 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -18,7 +18,7 @@ class Solution:
         islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 'L':  # If we find unvisited land ('L')
+                if grid[i][j] == 'L':  
                     dfs(grid, i, j)    
                     islands += 1       
         
