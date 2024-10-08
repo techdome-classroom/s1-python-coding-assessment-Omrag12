d22784ff1599ce72a5080c9856f4b0693bd051d9
om raghuwanshi <omrag103@gmail.com>
2024-10-08 12:28:15 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -12,7 +12,6 @@ def decode_message( s: str, p: str) -> bool:
         if p[j - 1] == '*':
             dp[0][j] = dp[0][j - 1]
 
-    # Fill the DP table
    for i in range(1, m + 1):
         for j in range(1, n + 1):
             if p[j - 1] == s[i - 1] or p[j - 1] == '?':
